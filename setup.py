68100b7004bb2fcb2dfde88202a34f69326ffa9e
David Villa Alises <david.villa@gmail.com>
2014-01-30 17:25:57 +0100

    modifications to support python-3.x by means of 2to3.

--- setup.py
+++ setup.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python
 
-try:
-    from distutils.core import setup
-except ImportError, excp:
-    from setuptools import setup
-    
-import pydot
+import sys
+from setuptools import setup
+
 import os
 
 os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
 
-setup(	name = 'pydot',
-    version = pydot.__version__,
+version = "1.0.32"
+
+config = dict(
+    name = 'pydot2',
+    version = version,
     description = 'Python interface to Graphviz\'s Dot',
     author = 'Ero Carrera',
     author_email = 'ero@dkbza.org',
     url = 'http://code.google.com/p/pydot/',
-    download_url = 'http://pydot.googlecode.com/files/pydot-%s.tar.gz' % pydot.__version__,
+    download_url = 'http://pydot.googlecode.com/files/pydot-%s.tar.gz' % version,
     license = 'MIT',
     keywords = 'graphviz dot graphs visualization',
     platforms = ['any'],
-    classifiers =	['Development Status :: 5 - Production/Stable',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    long_description = "\n".join(pydot.__doc__.split('\n')),
     py_modules = ['pydot', 'dot_parser'],
     install_requires = ['pyparsing', 'setuptools'],
-    data_files = [('.', ['LICENSE', 'README'])] )
+    data_files = [('.', ['LICENSE', 'README'])])
+
+
+if sys.version_info >= (3,):
+    config.update(dict(
+        use_2to3=True,
+    ))
+
+setup(**config)
