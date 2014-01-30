661b26a00bdb430be1109b54d4602f8e1e550d20
David Villa Alises <david.villa@gmail.com>
2014-01-30 15:31:29 +0100

    removing _nocomma internal pyparsing dependency

--- dot_parser.py
+++ dot_parser.py
@@ -25,8 +25,9 @@ from pyparsing import __version__ as pyparsing_version
 from pyparsing import ( nestedExpr, Literal, CaselessLiteral, Word, Upcase, OneOrMore, ZeroOrMore,
     Forward, NotAny, delimitedList, oneOf, Group, Optional, Combine, alphas, nums,
     restOfLine, cStyleComment, nums, alphanums, printables, empty, quotedString,
-    ParseException, ParseResults, CharsNotIn, _noncomma, dblQuotedString, QuotedString, ParserElement )
+    ParseException, ParseResults, CharsNotIn, dblQuotedString, QuotedString, ParserElement )
 
+_noncomma = "".join( [ c for c in printables if c != "," ] )
 
 class P_AttrList:
 
